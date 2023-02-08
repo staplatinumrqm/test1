4162e093f9823f0ac16a1b9a4e9b7e0d067b4d3b
staplatinumrqm <staplatinumrqm@maximcapsa@gmail.com>
2023-02-08 23:32:29 +0200

    shit

--- bash/testscript1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-case "$1" in 
-    gandon|GANDON)
-        echo "pidaras" ;;
-    pidr)
-        echo "sosat penis" ;;
-    *)
-        echo "usage: $0 start|stop" ; exit 1
-esac
--- bash/while_read.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-line=1
-while read LINE
-do 
-    echo "${line}: ${LINE}"
-    ((line++))
-done < /c/Users/Msi/Desktop/for\ 999/999\ fl.txt
