923c21e3eca2bb12a21a9c5392c3a96759a3f866
staplatinumrqm <staplatinumrqm@maximcapsa@gmail.com>
2023-02-11 19:10:04 +0200

    shit12

--- javadevops/src/main/java/com/example/Application.java
+++ javadevops/src/main/java/com/example/Application.java
@@ -18,7 +18,7 @@ public class Application {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("Java app started you faggot fucksss");
+        log.info("Java app started you faggot fucksssasd");
     }
 
     public String getStatus() {
